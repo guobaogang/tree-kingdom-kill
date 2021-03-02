ddf12899c933bd4b77f12c772a18b14cd799c10e
guobgf <guobgf@yonyou.com>
2021-03-02 20:08:07 +0800

    fix bug

--- server/src/server.js
+++ server/src/server.js
@@ -4,30 +4,39 @@ const port = 3000
 const httpServer = require("http").createServer(app)
 const io = require("socket.io")(httpServer)
 
+//所有已登录用户
 let connectedUsers = [];
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
+//socket连接
 io.on("connection", socket => {
-    console.log('a player connected');
+    updateUser();
     let userName = '';
+    //用户登录
     socket.on('login', (name, cb) => {
         userName = name;
+        console.log(userName + "加入连接")
         connectedUsers.push(name);
-        console.log(name + '  login');
         cb(true);
         updateUser();
     });
 
+    //广播更新用户列表
     function updateUser() {
         io.emit('updateUser', connectedUsers);
     }
 
+    //断开socket连接
     socket.on('disconnect', () => {
-        console.log('a player disconnect');
-        connectedUsers.splice(connectedUsers.indexOf(userName), 1);
+        console.log(userName + "断开连接")
+        let index = connectedUsers.indexOf(userName);
+        if (index > -1) {
+            connectedUsers.splice(index, 1);
+            updateUser();
+        }
     });
 })
 
