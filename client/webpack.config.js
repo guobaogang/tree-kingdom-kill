c7f4d9c1c7e88f280515536c77ed6d1e890c9756
guobgf <guobgf@yonyou.com>
2021-03-06 20:48:11 +0800

    init client

---
+++ client/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './index.html'
+        })
+    ]
+}
