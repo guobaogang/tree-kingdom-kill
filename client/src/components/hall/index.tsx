d9c904fc79bb56990806a57a8c2cb52e5aa837f5
guobgf <guobgf@yonyou.com>
2021-03-12 19:35:52 +0800

    update

--- client/src/components/hall/index.tsx
+++ client/src/components/hall/index.tsx
@@ -2,28 +2,42 @@ import React, { useEffect, useState } from "react";
 import socket from "../../model/socket";
 
 function Hall() {
-  const [roomList, setRoomList] = useState([]);
+    const [roomList, setRoomList] = useState([]);
+    const [roomName, setRoomName] = useState('');
+    const [userName, setUserName] = useState('');
 
-  useEffect(() => {
-    updateRooms();
-  }, []);
+    useEffect(() => {
+        updateRooms();
+    }, []);
 
-  const updateRooms = () => {
-    console.log("update rooms");
+    const updateRooms = () => {
+        console.log("update rooms");
 
-    socket.on("message", (msg) => {
-      console.log(msg);
-    });
-  };
+        socket.on("message", (msg: any) => {
+            console.log(msg);
+        });
+    };
 
-  return (
-    <div>
-      房间列表：
-      {roomList.map((room) => {
-        <div>room</div>;
-      })}
-    </div>
-  );
+    const createRoom = () => {
+        socket.emit('login', userName, roomName, (flag: Boolean) => {
+            if (flag) {
+                console.log('登陆成功');
+            }
+        })
+    }
+
+    return (
+        <div>
+            房间列表：
+            {roomList.map((room) => {
+                <div>room</div>;
+            })}
+            <input type="text" value={roomName} onChange={e => setRoomName(e.target.value)} name="roomName" />
+            <input type="text" value={userName} onChange={e => setUserName(e.target.value)} name="userName" />
+            <button onClick={createRoom}>创建房间</button>
+
+        </div>
+    );
 }
 
 export default Hall;
