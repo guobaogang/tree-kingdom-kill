b45a7dce512b286f9f6c3bd4e88fbc071614f498
guobgf <guobgf@yonyou.com>
2021-06-09 20:01:11 +0800

    router config

--- client/src/page/RouterPage/index.tsx
+++ client/src/page/RouterPage/index.tsx
@@ -1,19 +1,40 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Login from "../Login";
-import Hall from "../Hall";
-import Room from "../Room";
-import Register from "../Register";
-import './index.less';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import routerList from '../../router';
+import {getToken} from "../../utils/util";
 
 function RouterPage() {
+    const routerFun = (list: any[]) => {
+        let token = getToken();
+        return list.map(route => {
+            if (route.auth) {
+                if (token) {
+                    if (route.children) {//路由是否存在子路由
+                        /*return <Route key={route.name} path={route.path} render={() =>
+                            <route.component key={route.name}>
+                                {routerFun(route.children)}
+                                <Route key={route.name} exact path={route.path}
+                                       render={() => <Redirect to={route.redirect}/>}/>
+                            </route.component>
+                        }/>*/
+                    } else {//渲染路由
+                        return <Route key={route.name} exact path={route.path} component={route.component}/>
+                    }
+                } else {
+                    return <Redirect exact key={route.name} to='/Login'/>
+                }
+            } else {
+                return <Route key={route.name} exact path={route.path} component={route.component}/>
+            }
+        })
+    }
+
     return (
         <Router>
             <Switch>
-                <Route path="/" exact component={Login}/>
-                <Route path="/register" component={Register}/>
-                <Route path="/home" component={Hall}/>
-                <Route path="/room/:id" component={Room}/>
+                {
+                    routerFun(routerList)
+                }
             </Switch>
         </Router>
     )
---
+++ client/src/router/index.ts
@@ -0,0 +1,36 @@
+import Login from "../page/Login";
+import Hall from "../page/Hall";
+import Room from "../page/Room";
+import Register from "../page/Register";
+
+export default [
+    {
+        path: '/',
+        name: 'Login',
+        redirect: '/Login',
+        auth: false,
+        component: Login
+    }, {
+        path: '/login',
+        name: 'Login',
+        auth: false,
+        component: Login
+    }, {
+        path: '/register',
+        name: 'Register',
+        auth: false,
+        component: Register
+    },
+    {
+        path: '/home',
+        name: 'Hall',
+        auth: true,
+        component: Hall
+    },
+    {
+        path: '/room/:id',
+        name: 'Room',
+        auth: true,
+        component: Room
+    }
+]
