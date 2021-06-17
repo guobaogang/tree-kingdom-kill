a84715d92cdcb5914c2b9ba3d5bd3be3cd593583
guobgf <guobgf@yonyou.com>
2021-06-17 20:45:03 +0800

    路由跳转问题

--- client/src/page/RouterPage/index.tsx
+++ client/src/page/RouterPage/index.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
-import routerList from '../../router';
+//import routerList from '../../router';
 import {getToken} from "../../utils/util";
+import Login from "../Login";
+import Hall from "../Hall";
 
 function RouterPage() {
-    const routerFun = (list: any[]) => {
+    /*const routerFun = (list: any[]) => {
         let token = getToken();
         return list.map(route => {
             if (route.auth) {
                 if (token) {
                     if (route.children) {//路由是否存在子路由
-                        /*return <Route key={route.name} path={route.path} render={() =>
+                        /!*return <Route key={route.name} path={route.path} render={() =>
                             <route.component key={route.name}>
                                 {routerFun(route.children)}
                                 <Route key={route.name} exact path={route.path}
                                        render={() => <Redirect to={route.redirect}/>}/>
                             </route.component>
-                        }/>*/
+                        }/>*!/
                     } else {//渲染路由
                         return <Route key={route.name} exact path={route.path} component={route.component}/>
                     }
@@ -27,14 +29,18 @@ function RouterPage() {
                 return <Route key={route.name} exact path={route.path} component={route.component}/>
             }
         })
-    }
+    }*/
 
     return (
         <Router>
             <Switch>
-                {
+                {/*{
                     routerFun(routerList)
-                }
+                }*/}
+                <Route exact path="/login" render={() => <Login/>}/>
+                <Route path="/home" render={() => (
+                    getToken() ? <Hall/> : <Redirect to={"/login"}/>
+                )}/>
             </Switch>
         </Router>
     )
