8cc322b7f8280b11661bc0a8863baf7a0928a431
guobgf <guobgf@yonyou.com>
2021-06-23 19:42:51 +0800

    update

--- client/src/page/Login/index.tsx
+++ client/src/page/Login/index.tsx
@@ -2,19 +2,23 @@ import React, {useState} from "react";
 import {useHistory} from 'react-router-dom';
 import ajax from '../../api/ajax';
 import {setToken} from "../../utils/util";
-import {Button} from "antd";
 import './index.less';
+import {Form, Input, Button, Checkbox} from 'antd';
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
 
 function Login() {
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
     let history = useHistory();
 
-    const login = () => {
+    const register = () => {
+        history.replace('./register');
+    }
+
+    const onFinish = (values: any) => {
+        const {username, password} = values;
         ajax({
             url: 'api/user/login',
             data: {
-                userName,
+                userName: username,
                 password
             }
         })
@@ -28,24 +32,47 @@ function Login() {
             })
     }
 
-    const register = () => {
-        history.replace('./register');
-    }
-
     return (
-        <div className={'login'}>
-            userName: <input value={userName} onChange={e => setUserName(e.target.value)}/>
-            passWord: <input value={password} onChange={e => setPassword(e.target.value)}/>
-            <Button type={'primary'} onClick={login}>Login</Button>
-            <Button onClick={register}>Register</Button>
-            <div>
-                <ul>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                </ul>
-            </div>
+        <div className={'login-box-warp'}>
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{remember: true}}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="username"
+                    rules={[{required: true, message: 'Please input your Username!'}]}
+                >
+                    <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username"/>
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[{required: true, message: 'Please input your Password!'}]}
+                >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <a className="login-form-forgot" href="">
+                        Forgot password
+                    </a>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        Log in
+                    </Button>
+                    Or <a onClick={() => register()}>register now!</a>
+                </Form.Item>
+            </Form>
         </div>
     );
 }
